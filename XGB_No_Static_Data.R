88329611c33c1c93b38e66a9baa72110f11a3d93
Mika-Tal <mika.ida.tal@gmail.com>
2021-04-21 09:05:59 -0400

    XGB model with no static data

--- XGB_No_Static_Data.R
+++ XGB_No_Static_Data.R
@@ -1,5 +1,4 @@
-#Analysis for Random Forest and XGBoost
-#install.packages("xgboost")
+
 loadlibs = function(libs) {
   for(lib in libs) {
     class(lib)
@@ -13,23 +12,24 @@ loadlibs(libs)
 
 
 # Load Data Set
-original_df = read.csv("Data/shiny_merged_dataset_example.csv") %>% 
+NSD_original_df = read.csv("Data/shiny_merged_dataset_example.csv") %>% 
   mutate_at(c('week_first_date'), ~ as.Date(., "%Y-%m-%d")) %>% 
   mutate(two_week_forecast_date = week_first_date + 14) %>% 
   mutate_if(is.character, as.factor) %>% 
   select(-1)
 # Get latest date 
-latest_date = max(original_df$week_first_date)
+NSD_latest_date = max(NSD_original_df$week_first_date)
 #Two week outcome 
-twoWeek_df = original_df %>% 
+NSD_twoWeek_df = NSD_original_df %>% 
   select(week_first_date, State, Outcome.Variable) %>% 
   rename(two_week_outcome = Outcome.Variable,
          two_week_forecast_date = week_first_date) 
 #Merge with original to create a working df
-working_df = original_df %>% 
-  left_join(twoWeek_df,
+NSD_working_df = NSD_original_df %>% 
+  left_join(NSD_twoWeek_df,
             by = c("State" = "State",
-                   "two_week_forecast_date" = "two_week_forecast_date")) %>% 
+                   "two_week_forecast_date" = "two_week_forecast_date")) %>%
+  select(-c(6:18)) %>% 
   mutate(year = as.factor(year(two_week_forecast_date)),
          week = as.factor(week(two_week_forecast_date)))
 
@@ -38,40 +38,40 @@ set.seed(1)
 #Need if statement - Tobi
 #### If use shuffle prediction
 #generate random index list for training and test sets
-trainIndex = sample(1:nrow(working_df[working_df$two_week_forecast_date <= latest_date , ]),
-                    0.8 * nrow(working_df[working_df$two_week_forecast_date <= latest_date , ]))
+NSD_trainIndex = sample(1:nrow(NSD_working_df[NSD_working_df$two_week_forecast_date <= NSD_latest_date , ]),
+                    0.8 * nrow(NSD_working_df[NSD_working_df$two_week_forecast_date <= NSD_latest_date , ]))
 #create train set 
 #train = working_df[trainIndex, ]
 #create test set
 #test = working_df[-c(trainIndex), ]
 #train sequential
 #### If use sequential prediciton
-train = working_df[trainIndex, ] %>% na.omit()
+NSD_train = NSD_working_df[NSD_trainIndex, ] %>% na.omit()
 #test sequential
-test = working_df[-trainIndex, ] %>% na.omit()
+NSD_test = NSD_working_df[-NSD_trainIndex, ] %>% na.omit()
 
-pred = working_df[working_df$two_week_forecast_date > latest_date, ]
+NSD_pred = NSD_working_df[NSD_working_df$two_week_forecast_date > NSD_latest_date, ]
 
-pred_state = working_df[working_df$two_week_forecast_date > latest_date, ] %>% 
+NSD_pred_state = NSD_working_df[NSD_working_df$two_week_forecast_date > NSD_latest_date, ] %>% 
   filter(State == 'PA')
 
 #Convert the predictor variable matrices to xgb.DMatrix data types
-XGBTrain = xgb.DMatrix(data.matrix(train %>%
+NSD_XGBTrain = xgb.DMatrix(data.matrix(NSD_train %>%
                                      select(-c(two_week_forecast_date,
                                                two_week_outcome))))
-XGBTest = xgb.DMatrix(data.matrix(test %>%
+NSD_XGBTest = xgb.DMatrix(data.matrix(NSD_test %>%
                                     select(-c(two_week_forecast_date,
                                               two_week_outcome))))
-XGBPred = xgb.DMatrix(data.matrix(pred %>%
+NSD_XGBPred = xgb.DMatrix(data.matrix(NSD_pred %>%
                                     select(-c(two_week_forecast_date,
                                               two_week_outcome))))
 
-XGBPred_state = xgb.DMatrix(data.matrix(pred_state %>%
-                                    select(-c(two_week_forecast_date,
-                                              two_week_outcome))))
+NSD_XGBPred_state = xgb.DMatrix(data.matrix(NSD_pred_state %>%
+                                          select(-c(two_week_forecast_date,
+                                                    two_week_outcome))))
 
 #Define random grid parameters for tuning the models
-XGBtc=trainControl(
+NSD_XGBtc=trainControl(
   method='cv',
   number=5,
   allowParallel=TRUE,
@@ -79,7 +79,7 @@ XGBtc=trainControl(
   returnData=FALSE
 )
 # create grid for tuning hyperparameters
-XGBtg <- expand.grid(nrounds = c(50, 100, 250),
+NSD_XGBtg <- expand.grid(nrounds = c(50, 100, 250),
                      max_depth = c(5, 10, 15),
                      colsample_bytree = seq(0.5, 0.9, length.out = 5),
                      eta = c(0.1, 0.2, 0.4),
@@ -90,62 +90,63 @@ XGBtg <- expand.grid(nrounds = c(50, 100, 250),
 
 
 # train model - with dates and state 
-XGBModel = train(
-  XGBTrain, train$two_week_outcome,  
-  trControl = XGBtc,
-  tuneGrid = XGBtg,
+NSD_XGBModel = train(
+  NSD_XGBTrain, NSD_train$two_week_outcome,  
+  trControl = NSD_XGBtc,
+  tuneGrid = NSD_XGBtg,
   importance = TRUE,
   method = "xgbTree")
 
 # get hyperparameters of best model from grid search 
-XGBModel$bestTune
+NSD_XGBModel$bestTune
 
 #Predict
-XGBModel_test=predict(XGBModel, XGBTest)
+NSD_XGBModel_test=predict(NSD_XGBModel, NSD_XGBTest)
 
-XGBModel_pred = predict(XGBModel, XGBPred)
+NSD_XGBModel_pred = predict(NSD_XGBModel, NSD_XGBPred)
 
-XGBModel_state = predict(XGBModel, XGBPred_state)
+NSD_XGBModel_state = predict(NSD_XGBModel,NSD_XGBPred_state)
 
 #Model Performance
 #RMSE
-XGBRMSE = sqrt(mean ((test$two_week_outcome - XGBModel_test) ^ 2 ) )
+NSD_XGBRMSE = sqrt(mean ((NSD_test$two_week_outcome - NSD_XGBModel_test) ^ 2 ) )
 #Plot observed vs. predicted
-obs_vs_pred = test %>% 
+NSD_obs_vs_pred = NSD_test %>% 
   select(two_week_outcome) %>% 
   rename(Observed = two_week_outcome) %>% 
-  bind_cols(tibble(Predicted = XGBModel_test))
+  bind_cols(tibble(Predicted = NSD_XGBModel_test))
 
-obs_vs_pred_plot = ggplot(obs_vs_pred, aes(x = Observed, y = Predicted)) +
+obs_vs_pred_plot = ggplot(NSD_obs_vs_pred, aes(x = Observed, y = Predicted)) +
   geom_point() +
   geom_abline()
 
 
 #Plot Predictions
-outcome_all = train %>% 
+NSD_outcome_all = NSD_train %>% 
   select(two_week_forecast_date, two_week_outcome) %>%
   rename(Date = two_week_forecast_date,
          Outcome = two_week_outcome) %>% 
   mutate (Pred_vs_Obs = "Observed") %>% 
-  rbind(tibble(Date = pred$two_week_forecast_date,
-               Outcome = XGBModel_pred,
+  rbind(tibble(Date = NSD_pred$two_week_forecast_date,
+               Outcome = NSD_XGBModel_pred,
                Pred_vs_Obs = "Predicted"))
 
-outcome_plot_all = ggplot(outcome_all, aes(x = Date, y = Outcome))+
+outcome_plot_all = ggplot(NSD_outcome_all, aes(x = Date, y = Outcome))+
   geom_point(aes(color = Pred_vs_Obs))
 
-outcome_all_state = train %>% 
+NSD_outcome_all_state = NSD_train %>% 
   filter(State == 'PA') %>% 
   select(two_week_forecast_date, two_week_outcome) %>%
   rename(Date = two_week_forecast_date,
          Outcome = two_week_outcome) %>% 
   mutate (Pred_vs_Obs = "Observed") %>% 
-  rbind(tibble(Date = pred_state$two_week_forecast_date,
-               Outcome = XGBModel_state,
+  rbind(tibble(Date = NSD_pred_state$two_week_forecast_date,
+               Outcome = NSD_XGBModel_state,
                Pred_vs_Obs = "Predicted"))
 
-outcome_plot_state = ggplot(outcome_all_state, aes(x = Date, y = Outcome)) +
+NSD_outcome_plot_state = ggplot(NSD_outcome_all_state, aes(x = Date, y = Outcome)) +
   geom_point(aes(color = Pred_vs_Obs)) +
-  stat_smooth(method = 'lm', formula = y ~ poly(x,10), se = FALSE, color = "black")
+  stat_smooth(method = 'lm', formula = y ~ poly(x,10), se = TRUE, color = "darkgrey")
+
 
 
